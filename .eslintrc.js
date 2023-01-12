9da3241c3b8800067e4ee7099e41ff11b7cb82c9
huangshan <huangshan@coocaa.com>
2023-01-12 15:15:35 +0800

    eslint: 增加 eslint 配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "env": {
+    node: true
+  },
+  "extends": [
+    'plugin:vue/essential',
+  ],
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": 10,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "vue"
+  ],
+  "rules": {
+  }
+}
